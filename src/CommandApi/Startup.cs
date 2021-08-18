cae79ea22ec28ffc744c88265593ae54e194afe4
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-08-18 15:13:44 -0400

    Introduced User Secrets

--- src/CommandApi/Startup.cs
+++ src/CommandApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 
 namespace CommandApi
 {
@@ -24,7 +25,12 @@ namespace CommandApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserId"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
             services.AddScoped<ICommandRepo, SqlCommandRepo>();
             services.AddControllers();
         }
